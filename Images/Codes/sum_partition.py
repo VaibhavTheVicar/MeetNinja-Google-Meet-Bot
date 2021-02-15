b3fae4c4352448fb9a70ab522b551b3384aa4952
VaibhavTheVicar <77243855+VaibhavTheVicar@users.noreply.github.com>
2021-02-15 18:40:16 +0530

    Update sum_partition.py

--- Images/Codes/sum_partition.py
+++ Images/Codes/sum_partition.py
@@ -1,14 +1,16 @@
-def solver(arr,l,t,s=[]):
-	if sum(s)==t:
-		return True
-	if l == len(arr):
-		return False
-	s.append(arr[l])
-	if solver(arr,l+1,t):
-		return True
-	s.pop(-1)
-	if solver(arr,l+1,t):
-		return True
-	return False
-
-print(solver([1,3],0,))
+n = int(input())
+def div(n):
+    rs = (n*(n+1))//4
+    group1 = []
+    group2 = []
+    for i in range(n,0,-1):
+        if  rs - i >= 0:
+            group1.append(i)
+            rs-=i
+        else:
+            group2.append(i)
+    return (group1,group2)
+for i in range(n):
+    A =div(i)
+    print(i,"\n",sum(A[0]),A[0],sum(A[1]),A[1])
+#if rem is 0,3 ans 0 else 1
